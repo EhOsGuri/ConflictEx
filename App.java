c88ffc2c035831af2a71798a5354e584fbf4f8f3
Gabriel Vaz <gabriel.vaz@acad.pucrs.br>
2018-09-17 16:02:13 -0300

    Commit por Messi

--- App.java
+++ App.java
@@ -1,6 +1,8 @@
 //Classe editada por: Messi
-public static void main(String[]args){
-    public class App{
+public class App {
+
+	public static void main(String[] args) {
         System.out.println("Exemplo de Conflito - Messi")
-    }
+	}
+
 }
